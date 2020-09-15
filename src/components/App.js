24fbd5edc114e51eaeb3def5996b16c475eacd74
Kern Elder <kelel1@yahoo.com>
2020-09-15 13:26:26 -0400

    connect frontend to backend

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+  })
+})
+
+
+const query = gql`
+query {
+  allItems {
+    name
+    price
+    description
+  }
+}
+`
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data);
+  });
 
 const App = () => (
+  
   <div>
     <h1>
       Hello Kern
